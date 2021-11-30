a14775a0ea3f9657cf5ff8a01495029b25f6596c
Nuccino92 <anthnucci@hotmail.com>
2021-11-29 19:15:41 -0500

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFrdCGYES9GigjSEObtp905Z74XiuaRD4",
+  authDomain: "wheres-waldo-fec18.firebaseapp.com",
+  projectId: "wheres-waldo-fec18",
+  storageBucket: "wheres-waldo-fec18.appspot.com",
+  messagingSenderId: "1057992530849",
+  appId: "1:1057992530849:web:8a64ef42981ae0170889e0",
+};
+
+initializeApp(firebaseConfig);
