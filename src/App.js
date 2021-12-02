266d1fdf39b78f0f108e72dfa149bb135a1e46f9
Nuccino92 <anthnucci@hotmail.com>
2021-12-02 00:46:40 -0500

    set up paths along w/ chosen level prop drilling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/home/Home";
+import Game from "./components/game/Game";
+import { useState } from "react";
+
 function App() {
-  return <div className="App">dd</div>;
+  const [chosenLevel, setChosenLevel] = useState();
+
+  const handleClick = (level) => {
+    setChosenLevel(level);
+  };
+  // level={chosenLevel}
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home handleClick={handleClick} />} />
+          <Route path="/game" element={<Game level={chosenLevel} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
