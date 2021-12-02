8270e26d9e607024080ddc1681bf91db31244353
Nuccino92 <anthnucci@hotmail.com>
2021-12-02 00:47:14 -0500

    simple header setup

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-inner">
+        <div className="title-container">
+          <div style={{ color: "#489DD0" }} className="title">
+            WHERE'S
+          </div>
+          <div style={{ color: "#CA4141 " }} className="title">
+            WALDO
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
