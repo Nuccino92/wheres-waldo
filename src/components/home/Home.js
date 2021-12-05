5eb812e3a461b18434daabcdcaac0c3a7595606b
Nuccino92 <anthnucci@hotmail.com>
2021-12-04 23:18:44 -0500

    moved header component inside

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,10 @@
 import Slideshow from "./Slideshow";
+import Header from "../Header";
 
 const Home = ({ handleClick }) => {
   return (
     <div className="home">
+      <Header />
       <h1>Select Level</h1>
       <Slideshow handleClick={handleClick} />
     </div>
