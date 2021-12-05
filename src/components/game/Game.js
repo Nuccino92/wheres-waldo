36bede415a71b80b970fc2965dfcabdb95805e89
Nuccino92 <anthnucci@hotmail.com>
2021-12-04 23:19:33 -0500

    created game functionality

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,10 +1,85 @@
+import GameHeader from "./GameHeader";
+import Dropdown from "./Dropdown";
 import { Navigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
-const Game = (props) => {
-  return props.level === undefined ? (
+const Game = ({ level }) => {
+  const map = useRef();
+  const [xClicked, setxClicked] = useState();
+  const [yClicked, setyClicked] = useState();
+  const [dropdown, setDropdown] = useState(false);
+  const [event, setEvent] = useState(null);
+  const [chosenCharacter, setChosenCharacter] = useState(undefined);
+  const [charactersFound, setCharactersFound] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleClick = (e) => {
+    e.target.id === "map" ? setCoordinates(e) : setDropdown(false);
+  };
+
+  const dropdownCharacterPicked = (char) => {
+    setChosenCharacter(char);
+  };
+
+  useEffect(() => {
+    checkIfFound();
+    setChosenCharacter(undefined);
+  }, [chosenCharacter]);
+
+  const setCoordinates = (e) => {
+    setxClicked((e.nativeEvent.offsetX / e.target.offsetWidth) * 100);
+    setyClicked((e.nativeEvent.offsetY / e.target.offsetHeight) * 100);
+    setEvent(e);
+    setDropdown(true);
+  };
+
+  const checkIfFound = () => {
+    //if statement prevents gamebreak on page refresh from /game page
+    if (chosenCharacter === undefined) return;
+    let { x } = level[chosenCharacter];
+    let { y } = level[chosenCharacter];
+
+    if (
+      x > xClicked + 2.5 ||
+      x < xClicked - 2.5 ||
+      y > yClicked + 2.5 ||
+      y < yClicked - 2.5
+    ) {
+      return;
+    } else {
+      setCharactersFound((prev) => [...prev, chosenCharacter]);
+    }
+  };
+
+  const foundAll = () => {
+    setGameOver(true);
+  };
+
+  useEffect(() => {
+    const found = charactersFound.sort().toString();
+    //if statement prevents gamebreak on page refresh from /game page
+    if (charactersFound.length === 0) {
+      return;
+    } else if (found === [...level.characters].sort().toString()) {
+      foundAll();
+    }
+  }, [charactersFound]);
+
+  return level === undefined ? (
     <Navigate to="/" />
   ) : (
-    <div>this is the game</div>
+    <div className="game" onClick={(e) => handleClick(e)}>
+      <Dropdown
+        characterPicked={dropdownCharacterPicked}
+        characters={level.characters}
+        event={event}
+        active={dropdown}
+      />
+      <GameHeader level={level} gameOver={gameOver} />
+      <div className="level-map-container">
+        <img id={"map"} ref={map} src={level.map} alt="map of level"></img>
+      </div>
+    </div>
   );
 };
 
