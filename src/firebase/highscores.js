24d6021692a9c36a4af69d442ad0e2cf361031ef
Nuccino92 <anthnucci@hotmail.com>
2021-12-05 23:26:46 -0500

    added get and add highscores functions from firebase

---
+++ src/firebase/highscores.js
@@ -0,0 +1,27 @@
+import { database } from "./config";
+import { getDocs, collection, addDoc } from "firebase/firestore";
+
+const scores = collection(database, "highscores");
+
+export const highscores = () =>
+  getDocs(scores)
+    .then((snapshot) => {
+      let scores = [];
+      snapshot.docs.forEach((doc) => {
+        scores.push({ ...doc.data(), id: doc.id });
+      });
+      return scores;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+export const addHighscore = async (level, time, name) => {
+  const location = collection(database, "highscores");
+
+  addDoc(location, {
+    level: level.level,
+    time: time,
+    name: name,
+  });
+};
