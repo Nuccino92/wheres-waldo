c08a4638e5b59c11309903068b16db8e5bba1917
Nuccino92 <anthnucci@hotmail.com>
2021-12-06 21:59:49 -0500

    added div for background image use

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,6 +4,7 @@ import Header from "../Header";
 const Home = ({ handleClick }) => {
   return (
     <div className="home">
+      <div className="background-img"></div>
       <Header />
       <h1>Select Level</h1>
       <Slideshow handleClick={handleClick} />
