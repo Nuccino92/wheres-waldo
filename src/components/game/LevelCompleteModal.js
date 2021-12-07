f8d9b892bc8a5bfc3a11fc68233848c1b3bdd371
Nuccino92 <anthnucci@hotmail.com>
2021-12-06 21:59:35 -0500

    added check if games over to display modal

--- src/components/game/LevelCompleteModal.js
+++ src/components/game/LevelCompleteModal.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useRef } from "react";
 
-const LevelCompleteModal = ({ time, handleSubmitTime }) => {
+const LevelCompleteModal = ({ time, handleSubmitTime, runTime, gameOver }) => {
   const inputName = useRef();
 
   const handleClick = () => {
@@ -10,26 +10,34 @@ const LevelCompleteModal = ({ time, handleSubmitTime }) => {
     if (value === "") {
       return (current.className = "unfinished");
     } else current.className = "submitted";
-    handleSubmitTime(time, value);
+    handleSubmitTime(time, value, runTime);
   };
 
   return (
-    <div className="level-complete-modal">
+    <div
+      className={
+        gameOver ? "level-complete-modal active" : "level-complete-modal"
+      }
+    >
       <div className="modal-content">
         <div>You Finished In</div>
         <h2>{time}</h2>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">Enter name </label>
         <input
-          //   style={inputName.current.value === "" ? unfinished : style}
           name="name"
           ref={inputName}
           type="text"
           id="name"
+          maxLength="20"
         ></input>
-        <Link to="/">
-          <button>Back</button>
-        </Link>
-        <button onClick={handleClick}>Submit</button>
+        <div className="modal-button-container">
+          <Link to="/">
+            <button className="modal-btn">Back</button>
+          </Link>
+          <button className="modal-btn" onClick={handleClick}>
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
