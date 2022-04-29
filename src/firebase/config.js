12415a6861c14c1b63f8b6f2b620fc840695b01f
Anthony Nucci <86402204+Nuccino92@users.noreply.github.com>
2022-04-29 00:02:25 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log(process.env.REACT_APP_API_KEY);
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
